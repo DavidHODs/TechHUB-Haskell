65d0ab56e5755c77a0273e4a60b73ba77758a17a
DavidHODs <davidoluwatobi41@gmail.com>
2022-05-22 21:15:23 +0000

    keep getting a weird <<loop>> return

--- src/Init.hs
+++ src/Init.hs
@@ -6,19 +6,16 @@ import qualified System.Environment as Env
 localPG :: Pg.ConnectInfo
 localPG = Pg.defaultConnectInfo
     {
-        Pg.connectHost = envConnect "localhost" :: String,
+        Pg.connectHost = envConnect "host" :: String,
         Pg.connectDatabase = envConnect "hask" :: String,
         Pg.connectUser = envConnect "pguser" :: String,
         Pg.connectPassword = envConnect "pgpassword" :: String
     }
 
-envConnect :: String -> String 
-envConnect param = show (Env.getEnv "param")
+envConnect :: String -> String
+envConnect param =
+    show (Env.lookupEnv "host")
 
 instance Show a where
-    show :: IO String -> String
+    show :: a -> String
     show = show
-
-
-    -- instance Default ZonedTime where
-    --     def = Unsafe.read "2019-08-31 05:14:37.537084021 UTC"
