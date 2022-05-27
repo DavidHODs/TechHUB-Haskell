67d6678fc3d842e6ffdf8118ab44e85c20fd68f9
DavidHODs <davidoluwatobi41@gmail.com>
2022-05-27 11:31:24 +0000

    quack way to connect to postgres

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,12 @@ import Prelude
 
 import qualified API.EndPoint as EndPoint (app)
 import Init (localPG, envConnect, useEnv)
+import Model.Secret (secret)
 
 main :: IO ()
 main = do
     loadFile defaultConfig
-    conn <- Pg.connectPostgreSQL $ encodeUtf8 (pack useEnv)
+    -- secret is enclosed in a file ignored by git - it's a quack way to connect with postgres as I've not resolved working around the constraint of environment data returning IO String data type
+    conn <- Pg.connectPostgreSQL $ encodeUtf8 (pack secret)
     putStrLn "running on port 8080"
     run 8080 EndPoint.app
