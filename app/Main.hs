62f53f9da478ac7bb0f9218b8ec41d82f4531861
DavidHODs <davidoluwatobi41@gmail.com>
2022-05-27 13:38:34 +0000

    trying to set up auto migration

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,9 @@ module Main where
 
 import Network.Wai.Handler.Warp
 import qualified Database.PostgreSQL.Simple as Pg
-import Configuration.Dotenv (loadFile, defaultConfig)
+import qualified Database.PostgreSQL.Simple.Options as Op
+import qualified Database.PostgreSQL.Simple.Migration as Mg
+import qualified Configuration.Dotenv as Dotenv (loadFile, defaultConfig)
 import Data.Text.Encoding
 import Data.Text
 import Prelude
@@ -12,8 +14,9 @@ import Model.Secret (secret)
 
 main :: IO ()
 main = do
-    loadFile defaultConfig
+    Dotenv.loadFile Dotenv.defaultConfig
     -- secret is enclosed in a file ignored by git - it's a quack way to connect with postgres as I've not resolved working around the constraint of environment data returning IO String data type
     conn <- Pg.connectPostgreSQL $ encodeUtf8 (pack secret)
+    Mg.runMigration conn Mg.defaultOptions $ Mg.MigrationValidation (Mg.MigrationDirectory "Database/Postgres")
     putStrLn "running on port 8080"
     run 8080 EndPoint.app
